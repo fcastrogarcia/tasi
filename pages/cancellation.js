7c0f8f24d9aaf6867aa4ed3893cf08f4df032a1f
Castro Garcia <f.a.castro.garcia@accenture.com>
2021-03-03 16:51:22 -0300

    Add cancellation view

--- pages/cancellation.js
+++ pages/cancellation.js
@@ -1,5 +1,24 @@
+import Layout from "components/Layout";
+import Typography from "@material-ui/core/Typography";
+import useSetTimeout from "hooks/useSetTimeout";
+import { useRouter } from "next/router";
+
 const Cancellation = () => {
-  return <div className="layout">Cancellation</div>;
+  const router = useRouter();
+  useSetTimeout(() => router.push("/"), 5000);
+
+  return (
+    <Layout>
+      <div
+        className="container"
+        style={{ flexDirection: "column", justifyContent: "center" }}
+      >
+        <Typography variant="h4" gutterBottom>
+          La operación ha sido cancelada.
+        </Typography>
+      </div>
+    </Layout>
+  );
 };
 
 export default Cancellation;

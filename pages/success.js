306b05a72a8703bf87b3e74cc4b534f86c31b364
Castro Garcia <f.a.castro.garcia@accenture.com>
2021-03-04 16:58:14 -0300

    Fix amount formatting in succes page

--- pages/success.js
+++ pages/success.js
@@ -2,6 +2,7 @@ import Layout from "components/Layout";
 import useSetTimeout from "hooks/useSetTimeout";
 import { useRouter } from "next/router";
 import Typography from "@material-ui/core/Typography";
+import NumberFormat from "react-number-format";
 
 const SuccessPage = () => {
   const router = useRouter();
@@ -15,8 +16,17 @@ const SuccessPage = () => {
     <Layout>
       <div className="container" style={{ padding: "0 24px" }}>
         <Typography variant="h5" align="center">
-          Su {operation === "extraction" ? "extracción" : "depósito"} de monto $
-          {amount}, en la cuenta N° XXXX XXXX XXX, fue realizado con éxito.
+          Su {operation === "extraction" ? "extracción" : "depósito"} de monto{" "}
+          {
+            <NumberFormat
+              value={amount || 0}
+              displayType={"text"}
+              decimalSeparator={","}
+              thousandSeparator={"."}
+              prefix={"$"}
+            />
+          }
+          , en la cuenta N° XXXX XXXX XXX, fue realizado con éxito.
         </Typography>
       </div>
     </Layout>
